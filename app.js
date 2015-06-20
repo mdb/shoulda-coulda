8996cae29e35a83b9cc059bcf1fb2f58fae22666
Mike Ball <mikedball@gmail.com>
2015-06-20 13:59:13 -0400

    second pass with API backend

--- app.js
+++ app.js
@@ -1,18 +1,19 @@
 var SlackBot = require('slackbots'),
-    redis = require('redis').createClient(),
+    request = require('request'),
     bot = new SlackBot({
-      token: process.env.CLAUDE_TV_TOKEN,
-      name: 'claude_tv'
+      token: process.env.SHOULDA_COULDA_TOKEN,
+      name: 'shoulda_coulda'
     }),
     params = {
       icon_emoji: ':cat:'
-    };
+    },
+    endpoint = process.env.SHOULD_COUNTER_API_ENDPOINT
 
 bot.on('start', handleStart);
 bot.on('message', handleMessage);
 
 function handleStart(data) {
-  bot.postMessageToChannel('bottest', 'Hey!', params);
+  bot.postMessageToChannel((process.env.SHOULDA_COULDA_CHANNEL || 'bottest'), 'Hey!', params);
 }
 
 function handleMessage(data) {
@@ -20,19 +21,44 @@ function handleMessage(data) {
 
   if (!shouldRespond(json)) { return false; }
 
-  redis.incr('shouldCount');
-  redis.get('shouldCount', reportCount);
+  console.log(json);
+
+  saveCount(json, reportCount);
 }
 
-function reportCount(err, reply) {
-  if (err) console.log(err);
+function saveCount(json, callback) {
+  request.post({
+    url: endpoint,
+    form: {
+      should: {
+        user: json.user,
+        context: json.text
+      }
+    }
+  }, function(err, resp, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log(body);
+    }
 
-  bot.postMessageToChannel('bottest', '"should" has been said ' + reply.toString() + ' times in this channel since I was born', params);
+    callback();
+  });
+}
+
+function reportCount() {
+  request(endpoint, function(err, resp, body) {
+    if (err) {
+      console.log(err);
+    } else {
+      bot.postMessageToChannel('bottest', '"should" has been said ' + JSON.parse(body).length.toString() + ' times in this channel since I was born', params);
+    }
+  });
 }
 
 function shouldRespond(data) {
   return data.text &&
-         data.username !== 'claude_tv' &&
+         data.username !== bot.name &&
          includesShould(data.text);
 }
 
